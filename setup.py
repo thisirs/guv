bc8e75524eca847147d9934791d39419ea5433d9
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2019-05-07 09:06:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from setuptools import setup
 
 setup(name='doit_utc',
       version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      author='Sylvain Rousseau',
+      author_email='sylvain.rousseau@hds.utc.fr',
       license='MIT',
       packages=['doit_utc'],
       zip_safe=False)
