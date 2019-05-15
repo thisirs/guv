8808ad325e8543cd8d638683831105f1ad04293b
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2019-05-15 17:03:29 +0200

    Add entrypoint

---
+++ doit_utc/runner.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from doit.cmd_base import ModuleTaskLoader
+from doit.doit_cmd import DoitMain
+
+# Set configuration files to load
+os.environ.setdefault(
+    "SETTINGS_FILES",
+    ",".join([os.path.abspath("../config.py"), os.path.abspath("./config.py")]),
+)
+
+from .config import settings
+
+DOIT_CONFIG = settings.DOIT_CONFIG
+
+
+from .dodo_instructors import *
+from .dodo_utc import *
+from .dodo_grades import *
+from .dodo_students import *
+
+
+def main():
+    if len(sys.argv) >= 2 and sys.argv[1] == "doit":
+        del sys.argv[1]
+        sys.exit(DoitMain(ModuleTaskLoader(globals())).run(sys.argv[1:]))
+    else:
+        sys.exit(DoitMain(ModuleTaskLoader(globals())).run([sys.argv[1]]))
--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(name='doit_utc',
       version='0.1',
       author='Sylvain Rousseau',
       author_email='sylvain.rousseau@hds.utc.fr',
+      entry_points={
+          'console_scripts': ['doit-utc=doit_utc.runner:main']
+      },
       license='MIT',
       packages=['doit_utc'],
       zip_safe=False)
