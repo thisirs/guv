c9fef2a511a23a6f4a6408def8aee57793bc2474
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2020-02-13 10:20:16 +0100

    GROUP_ID is now defined in config.py

--- doit_utc/scripts/moodle_date.py
+++ doit_utc/scripts/moodle_date.py
@@ -1,68 +1,15 @@
 from datetime import datetime, date
 
-GROUP_ID = {
-    'C1': 5377,
-    'C2': 5378,
-    'D1': 5379,
-    'D10': 5388,
-    'D11': 5389,
-    'D12': 5390,
-    'D2': 5380,
-    'D3': 5381,
-    'D4': 5382,
-    'D5': 5383,
-    'D6': 5384,
-    'D7': 5385,
-    'D8': 5386,
-    'D9': 5387,
-    'T1A': 5359,
-    'T1Ai': 5391,
-    'T1Aii': 5397,
-    'T1B': 5371,
-    'T1Bi': 5403,
-    'T1Bii': 5409,
-    'T2A': 5360,
-    'T2Ai': 5392,
-    'T2Aii': 5398,
-    'T2B': 5372,
-    'T2Bi': 5404,
-    'T2Bii': 5410,
-    'T3A': 5361,
-    'T3Ai': 5393,
-    'T3Aii': 5399,
-    'T3B': 5373,
-    'T3Bi': 5405,
-    'T3Bii': 5411,
-    'T4A': 5362,
-    'T4Ai': 5394,
-    'T4Aii': 5400,
-    'T4B': 5374,
-    'T4Bi': 5406,
-    'T4Bii': 5412,
-    'T5A': 5363,
-    'T5Ai': 5395,
-    'T5Aii': 5401,
-    'T5B': 5375,
-    'T5Bi': 5407,
-    'T5Bii': 5413,
-    'T6A': 5364,
-    'T6Ai': 5396,
-    'T6Aii': 5402,
-    'T6B': 5376,
-    'T6Bi': 5408,
-    'T6Bii': 5414
-}
-
 
 class Cond:
     def __init__(self, sts=[]):
         self.visible = False
         self.sts = sts
 
-    def to_PHP(self):
-        return CondAnd(sts=[self]).to_PHP()
+    def to_PHP(self, group_id):
+        return CondAnd(sts=[self]).to_PHP(group_id)
 
-    def to_PHP_inner(self):
+    def to_PHP_inner(self, group_id):
         raise NotImplementedError('Abstract method')
 
     def __and__(self, other):
@@ -108,7 +55,7 @@ class CondDate(Cond):
         else:
             raise Exception()
 
-    def to_PHP_inner(self):
+    def to_PHP_inner(self, group_id):
         if type(self.dt) == date:
             dt = datetime.combine(self.dt, datetime.min.time())
         else:
@@ -129,8 +76,8 @@ class CondGroup(Cond):
         else:
             raise Exception()
 
-    def to_PHP_inner(self):
-        id = GROUP_ID[self.grp]
+    def to_PHP_inner(self, group_id):
+        id = group_id[self.grp]
         return {'type': 'group', 'id': id}
 
 
@@ -142,8 +89,8 @@ class CondCompound(Cond):
     def __not__(self):
         self.negate = not self.negate
 
-    def to_PHP(self):
-        e = self.to_PHP_inner()
+    def to_PHP(self, group_id):
+        e = self.to_PHP_inner(group_id)
         if (isinstance(self, CondAnd) and not self.negate) or \
            (isinstance(self, CondOr) and self.negate):
             key = 'showc'
@@ -158,14 +105,14 @@ class CondCompound(Cond):
 
 
 class CondOr(CondCompound):
-    def to_PHP_inner(self):
-        sts_PHP = [e.to_PHP_inner() for e in self.sts]
+    def to_PHP_inner(self, group_id):
+        sts_PHP = [e.to_PHP_inner(group_id) for e in self.sts]
         op = '!|' if self.negate else '|'
         return {'op': op, 'c': sts_PHP}
 
 
 class CondAnd(CondCompound):
-    def to_PHP_inner(self):
+    def to_PHP_inner(self, group_id):
         op = '!&' if self.negate else '&'
-        sts_PHP = [e.to_PHP_inner() for e in self.sts]
+        sts_PHP = [e.to_PHP_inner(group_id) for e in self.sts]
         return {'op': op, 'c': sts_PHP}
