5671c89c1e3e5345e89929d916091374224edc50
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2020-09-27 15:50:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,41 @@
 from setuptools import setup
 
-setup(name='doit_utc',
-      version='0.1',
-      author='Sylvain Rousseau',
-      author_email='sylvain.rousseau@hds.utc.fr',
-      entry_points={
-          'console_scripts': ['doit-utc=doit_utc.runner:main']
-      },
-      license='MIT',
-      packages=['doit_utc'],
-      zip_safe=False,
-      install_requires=[
-          "datetime",
-          "doit",
-          "icalendar",
-          "jinja2",
-          "latex",
-          "markdown",
-          "numpy",
-          "openpyxl",
-          "pandas",
-          "pynliner",
-          "PyPDF2",
-          "tabula-py",
-          "unidecode",
-          "oyaml",
-          "aiohttp",
-          "browser_cookie3",
-          "xlrd",
-          "schema"
-      ]
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="doit_utc",
+    version="0.1",
+    author="Sylvain Rousseau",
+    author_email="sylvain.rousseau@hds.utc.fr",
+    description="Programme d'aide à la gestion d'une UV",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={"console_scripts": ["doit-utc=doit_utc.runner:main"]},
+    license="MIT",
+    packages=["doit_utc"],
+    zip_safe=False,
+    install_requires=[
+        "datetime",
+        "doit",
+        "icalendar",
+        "jinja2",
+        "latex",
+        "markdown",
+        "numpy",
+        "openpyxl",
+        "pandas",
+        "pynliner",
+        "PyPDF2",
+        "tabula-py",
+        "unidecode",
+        "oyaml",
+        "aiohttp",
+        "browser_cookie3",
+        "xlrd",
+        "schema",
+    ],
 )
