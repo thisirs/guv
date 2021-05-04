7585e0b82d189cfcc2645a69b2df28e9d4a7afd4
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2021-05-04 22:15:55 +0200

    Add useful functions in __init__.py

---
+++ src/guv/__init__.py
@@ -0,0 +1,13 @@
+from .utils import (
+    aggregate,
+    aggregate_org,
+    compute_new_column,
+    replace_column,
+    replace_regex,
+    skip_range,
+    skip_week,
+    slugrot,
+    switch,
+)
+
+from .utils_config import fillna_column
