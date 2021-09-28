9a6bc8c1c8f257abbd14d1f95bbff6abcd36eda7
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2021-09-28 14:08:39 +0200

    Add more examples in CsvCreateGroups docstring

--- src/guv/tasks/moodle.py
+++ src/guv/tasks/moodle.py
@@ -731,7 +731,7 @@ class CsvCreateGroups(UVTask, CliArgsMixin):
     exclusives ``--proportions``, ``--group-size`` et
     ``--num-groups``.
 
-    Le nom des groupes est controlé par ``--template``. Les
+    Le nom des groupes est controlé par l'option ``--template``. Les
     remplacements suivants sont disponibles à l'intérieur de
     ``--template`` :
 
@@ -774,12 +774,28 @@ class CsvCreateGroups(UVTask, CliArgsMixin):
        guv csv_create_groups Projet1 -G TD --group-size 3
 
 
+    Faire des trinomes à l'intérieur de chaque sous-groupe de TD sans
+    qu'aucun des trinomes n'ait déjà été choisi dans la colonne
+    ``Projet1`` :
+
+    .. code:: bash
+
+       guv csv_create_groups Projet2 -G TD --group-size 3 --other-groups Projet1
+
+
     Partager en deux chaque sous-groupe de TD avec des noms de groupes
-    de la former D1i, D1ii, D2i, D2ii... :
+    de la forme D1i, D1ii, D2i, D2ii... :
 
     .. code:: bash
 
        guv csv_create_groups HalfGroup -G TD --proportions .5 .5 --template '{grouping_name}{group_name}' --names i ii
+
+    Partager l'effectif en deux parties selon l'ordre alphabétique
+    avec les noms de groupes ``First`` et ``Second`` :
+
+    .. code:: bash
+
+       guv csv_create_groups Half --proportions .5 .5 --ordered --names First Second --template '{group_name}'
 
     """
 
