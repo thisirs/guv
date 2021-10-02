37d4242f5555f070ccfa5bd6bec5ae3d76eb0206
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2021-10-02 11:19:33 +0200

    More examples in PdfAttendance

--- src/guv/tasks/attendance.py
+++ src/guv/tasks/attendance.py
@@ -62,9 +62,29 @@ class PdfAttendance(UVTask, CliArgsMixin):
     Examples
     --------
 
-    .. code:: bash
+    - Feuille de présence nominative :
 
-       guv pdf_attendance --title "Examen de TP" --group TP
+      .. code:: bash
+
+         guv pdf_attendance --title "Examen"
+
+    - Feuilles de présence nominative par groupe de TP :
+
+      .. code:: bash
+
+         guv pdf_attendance --title "Examen de TP" --group TP
+
+    - Feuilles de présence sans les noms par groupe de TP :
+
+      .. code:: bash
+
+         guv pdf_attendance --title "Examen de TP" --group TP --blank
+
+    - Feuilles de présence nominative découpées pour trois salles :
+
+      .. code:: bash
+
+         guv pdf_attendance --title "Examen de TP" --count 24 24 24 --name \"Salle 1\" \"Salle 2\" \"Salle 3\"
 
     """
 
