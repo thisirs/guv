2c3a615ab8e532ecc80e5bd9e220fb1321d974bd
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2022-01-14 19:47:48 +0100

    Output log message to stdout

--- src/guv/config.py
+++ src/guv/config.py
@@ -276,7 +276,7 @@ class LogFormatter(logging.Formatter):
             record.levelno, self._fmt) % record.__dict__
 
 logger = logging.getLogger("guv")
-handler = logging.StreamHandler()
+handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(LogFormatter())
 logger.setLevel(settings.DEBUG)
 logger.addHandler(handler)
