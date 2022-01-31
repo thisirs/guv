b1139216ec98ab2b19fc13396617b722855f589e
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2022-01-31 13:00:47 +0100

    Test passed arguments for PdfAttendance

--- tests/test_pdf_attendance.py
+++ tests/test_pdf_attendance.py
@@ -5,5 +5,26 @@ from conftest import path_dependency
 @path_dependency("test_xls_student_data")
 def test_pdf_attendance(guv):
     guv.cd("A2020", "SY02")
+    guv("pdf_attendance").succeed()
+    assert (guv.cwd / "generated" / "Feuille_de_présence_all.pdf").is_file()
+
+
+@path_dependency("test_xls_student_data")
+def test_pdf_attendance2(guv):
+    guv.cd("A2020", "SY02")
+    guv("pdf_attendance -t foo").succeed()
+    assert (guv.cwd / "generated" / "foo_all.pdf").is_file()
+
+
+@path_dependency("test_xls_student_data")
+def test_pdf_attendance3(guv):
+    guv.cd("A2020", "SY02")
+    guv("pdf_attendance -g TD").succeed()
+    assert (guv.cwd / "generated" / "Feuille_de_présence_TD.zip").is_file()
+
+
+@path_dependency("test_xls_student_data")
+def test_pdf_attendance4(guv):
+    guv.cd("A2020", "SY02")
     guv("pdf_attendance -t foo -g TD").succeed()
-    assert (guv.cwd / "generated" / "attendance_TD.zip").is_file()
+    assert (guv.cwd / "generated" / "foo_TD.zip").is_file()
