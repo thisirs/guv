68f2670505fac3177313c9160d4fe26e90792c72
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2022-02-01 10:17:59 +0100

    Add test with no group

--- tests/test_pdf_attendance_full.py
+++ tests/test_pdf_attendance_full.py
@@ -3,7 +3,16 @@ from conftest import path_dependency
 
 
 @path_dependency("test_xls_student_data")
-def test_pdf_attendance_full(guv):
+def test_pdf_attendance_full(guv, guvcapfd):
+    guv.cd("A2020", "SY02")
+    guv("pdf_attendance_full -n 14").succeed()
+    assert (guv.cwd / "generated" / "attendance_all_full.zip").is_file()
+    guvcapfd.stdout_search(".  pdf_attendance_full")
+
+
+@path_dependency("test_xls_student_data")
+def test_pdf_attendance_full2(guv, guvcapfd):
     guv.cd("A2020", "SY02")
     guv("pdf_attendance_full -g TD -n 14").succeed()
     assert (guv.cwd / "generated" / "attendance_TD_full.zip").is_file()
+    guvcapfd.stdout_search(".  pdf_attendance_full")
