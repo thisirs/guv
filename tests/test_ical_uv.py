498b6b57bb45650b83f551730ca10a6352cfe897
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2022-02-01 10:43:08 +0100

    Add tests/test_ical_uv.py

---
+++ tests/test_ical_uv.py
@@ -0,0 +1,19 @@
+import pytest
+from conftest import path_dependency
+
+
+@path_dependency("test_planning_slots")
+def test_ical_uv(guv, guvcapfd):
+    guv.cd("A2020", "SY02")
+    guv("ical_uv").succeed()
+    assert (guv.cwd / "documents" / "ics.zip").is_file()
+    guvcapfd.stdout_search(".  ical_uv")
+
+
+@path_dependency("test_planning_slots")
+def test_ical_uv0(guv, guvcapfd):
+    guv.cd("A2020")
+    guv("ical_uv").succeed()
+    assert (guv.cwd / "SY02" / "documents" / "ics.zip").is_file()
+    assert (guv.cwd / "SY09" / "documents" / "ics.zip").is_file()
+    guvcapfd.stdout_search(".  ical_uv")
