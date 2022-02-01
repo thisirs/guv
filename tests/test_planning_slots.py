b474cc9425f1bfd64a65d9272b19398cce8f808f
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2022-02-01 10:18:18 +0100

    Use class in tests/test_planning_slots.py

--- tests/test_planning_slots.py
+++ tests/test_planning_slots.py
@@ -11,67 +11,90 @@ def test_planning_slots0(guv, guvcapfd):
     )
 
 
-@path_dependency("test_week_slots")
-def test_planning_slots(guv, xlsx):
-    guv.cd("A2020")
-    guv.change_config(
-        """
-        from datetime import date
-        from guv.helpers import skip_week, skip_range
+@path_dependency("test_week_slots", name="test_planning_slots")
+class TestPlanningSlots:
+    def test_planning_slots0(self, guv, xlsx, guvcapfd):
+        guv.cd("A2020")
+        guv.change_config(
+            """
+            from datetime import date
+            from guv.helpers import skip_week, skip_range
 
-        # Jours fériés
-        ferie = [date(2020, 10, 15), date(2020, 11, 11)]
+            # Jours fériés
+            ferie = [date(2020, 10, 15), date(2020, 11, 11)]
 
-        # Première semaine sans TD/TP
-        debut = skip_week(date(2020, 9, 7))
+            # Première semaine sans TD/TP
+            debut = skip_week(date(2020, 9, 7))
 
-        # Semaine des médians
-        median = skip_range(date(2020, 11, 3), date(2020, 11, 9))
+            # Semaine des médians
+            median = skip_range(date(2020, 11, 3), date(2020, 11, 9))
 
-        # Vacances
-        vacances_toussaint = skip_range(date(2020, 10, 26), date(2020, 10, 31))
-        vacances_noel = skip_range(date(2020, 12, 23), date(2021, 1, 3))
-        vacances = vacances_toussaint + vacances_noel
+            # Vacances
+            vacances_toussaint = skip_range(date(2020, 10, 26), date(2020, 10, 31))
+            vacances_noel = skip_range(date(2020, 12, 23), date(2021, 1, 3))
+            vacances = vacances_toussaint + vacances_noel
 
-        # Semaine des finals
-        final = skip_range(date(2021, 1, 9), date(2021, 1, 16))
+            # Semaine des finals
+            final = skip_range(date(2021, 1, 9), date(2021, 1, 16))
 
-        PLANNINGS={
-            "A2020": {
-                "UVS": ["SY09", "SY02"],
-                "PL_BEG": date(2020, 9, 7),
-                "PL_END": date(2021, 1, 16),
-                "TURN": {
-                    date(2020, 10, 19): "Jeudi",
-                    date(2020, 11, 10): "Mercredi",
-                    date(2020, 12, 23): "Samedi",
-                },
-                "SKIP_DAYS_C": ferie + vacances + median + final,
-                "SKIP_DAYS_D": ferie + vacances + debut + median + final,
-                "SKIP_DAYS_T": ferie + vacances + debut + final
+            PLANNINGS={
+                "A2020": {
+                    "UVS": ["SY09", "SY02"],
+                    "PL_BEG": date(2020, 9, 7),
+                    "PL_END": date(2021, 1, 16),
+                    "TURN": {
+                        date(2020, 10, 19): "Jeudi",
+                        date(2020, 11, 10): "Mercredi",
+                        date(2020, 12, 23): "Samedi",
+                    },
+                    "SKIP_DAYS_C": ferie + vacances + median + final,
+                    "SKIP_DAYS_D": ferie + vacances + debut + median + final,
+                    "SKIP_DAYS_T": ferie + vacances + debut + final
+                }
             }
-        }
-        """
-    )
+            """
+        )
 
-    guv("planning_slots").succeed()
-    assert (guv.cwd / "SY02" / "generated" / "planning.xlsx").is_file()
-    assert (guv.cwd / "SY09" / "generated" / "planning.xlsx").is_file()
+        guv("planning_slots").succeed()
+        guvcapfd.stdout_search(
+            ".  planning_slots"
+        )
+        assert (guv.cwd / "SY02" / "generated" / "planning.xlsx").is_file()
+        assert (guv.cwd / "SY09" / "generated" / "planning.xlsx").is_file()
 
-    doc = xlsx(guv.cwd / "SY02" / "generated" / "planning.xlsx")
-    doc.columns(
-        "Activité",
-        "Jour",
-        "Heure début",
-        "Heure fin",
-        "Semaine",
-        "Locaux",
-        "Type créneau",
-        "Lib. créneau",
-        "Intervenants",
-        "Responsable",
-        "date",
-        "num",
-        "numAB",
-        "nweek",
-    )
+        doc = xlsx(guv.cwd / "SY02" / "generated" / "planning.xlsx")
+        doc.columns(
+            "Activité",
+            "Jour",
+            "Heure début",
+            "Heure fin",
+            "Semaine",
+            "Locaux",
+            "Type créneau",
+            "Lib. créneau",
+            "Intervenants",
+            "Responsable",
+            "date",
+            "num",
+            "numAB",
+            "nweek",
+        )
+
+    def test_planning_slots1(self, guv, xlsx, guvcapfd):
+        guv("planning_slots").succeed()
+        guvcapfd.stdout_search(
+            "-- planning_slots"
+        )
+
+        guv.change_config(
+            """PLANNINGS["A2020"]["PL_BEG"] = date(2020, 9, 14)"""
+        )
+        guv("planning_slots").succeed()
+        guvcapfd.stdout_search(
+            ".  planning_slots"
+        )
+
+        guv("planning_slots").succeed()
+        guvcapfd.stdout_search(
+            "-- planning_slots"
+        )
