491d3dd33f8bab849c9d776d4bb042bbd7093338
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2022-06-06 15:43:53 +0200

    Add tests/test_html_inst.py

---
+++ tests/test_html_inst.py
@@ -0,0 +1,10 @@
+import pytest
+from tests.plugins.test_path import path_dependency
+
+
+@path_dependency("test_week_slots")
+def test_html_inst(guv, guvcapfd):
+    guv.cd(guv.semester, guv.uvs[0])
+    guv("html_inst").succeed()
+    assert (guv.cwd / "generated" / "intervenants.html").is_file()
+    guvcapfd.stdout_search(".  html_inst")
