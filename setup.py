4c7df4b22179f72ba8da76c8992bc6ba2fc4e830
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2023-03-21 12:00:43 +0100

    Bump version to 0.14

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="guv",
-    version="0.13.1",
+    version="0.14",
     author="Sylvain Rousseau",
     author_email="sylvain.rousseau@hds.utc.fr",
     description="Programme d'aide à la gestion d'UV",
