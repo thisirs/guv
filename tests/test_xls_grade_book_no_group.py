19de459bde53589f968e339d92ada0c8aef72ffe
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2024-06-21 22:56:34 +0200

    Update tests

--- tests/test_xls_grade_book_no_group.py
+++ tests/test_xls_grade_book_no_group.py
@@ -3,11 +3,24 @@ from tests.plugins.test_path import path_dependency
 
 
 @path_dependency("test_xls_student_data")
-def test_xls_grade_book_no_group(guv):
+def test_xls_grade_book_no_group_1(guv):
     uv = guv.uvs[0]
     guv.cd(guv.semester, uv)
-    guv.copy_file("config_gradebook_test.yaml", "documents")
+    guv.copy_file("config_gradebook_test1.yaml", "documents")
+    guv.copy_file("config_gradebook_test2.yaml", "documents")
     guv(
-        "xls_grade_book_no_group --name Test --marking-scheme documents/config_gradebook_test.yaml"
+        "xls_grade_book_no_group --name Test --marking-scheme documents/config_gradebook_test1.yaml,documents/config_gradebook_test2.yaml"
+    ).succeed()
+    assert (guv.cwd / "generated" / "Test_gradebook.xlsx").is_file()
+
+
+@path_dependency("test_xls_student_data")
+def test_xls_grade_book_no_group_2(guv):
+    uv = guv.uvs[0]
+    guv.cd(guv.semester, uv)
+    guv.copy_file("config_gradebook_test1.yaml", "documents")
+    guv.copy_file("config_gradebook_test2.yaml", "documents")
+    guv(
+        "xls_grade_book_no_group --name Test --marking-scheme documents/config_gradebook_test1.yaml,documents/config_gradebook_test2.yaml"
     ).succeed()
     assert (guv.cwd / "generated" / "Test_gradebook.xlsx").is_file()
