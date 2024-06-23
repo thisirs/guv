f365fa4b77ff0fb889646f4269d9cd22b7acbac0
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2024-06-23 20:16:59 +0200

    Improve docstring

--- src/guv/helpers.py
+++ src/guv/helpers.py
@@ -905,17 +905,17 @@ class AggregateSelf(Operation):
     """Agrégation du fichier central ``effectif.xlsx`` lui-même.
 
     Il est parfois plus pratique d'ajouter soi-même des colonnes dans le fichier
-    central ``effectif.xlsx`` au lieu d'en ajouter programmatiquement via
-    ``DOCS.aggregate(...)`` par exemple. Comme *guv* ne peut pas détecter de
-    façon fiable les colonnes ajoutées, il faut lui indiquer lesquelles ont été
-    manuellement ajoutées et qu'il faut garder lors de la mise à jour du fichier
-    central ``effectif.xlsx``.
+    central ``effectif.xlsx`` ou d'en modifier d'autres au lieu d'en ajouter
+    programmatiquement via ``DOCS.aggregate(...)`` par exemple. Comme *guv* ne
+    peut pas détecter de façon fiable les colonnes ajoutées ou modifiées, il
+    faut lui indiquer lesquelles via l'argument ``*columns``.
 
     Parameters
     ----------
 
     *columns : any number of :obj:`str`
-        Les colonnes manuellement ajoutées à garder lors de la mise à jour du fichier central.
+        Les colonnes manuellement ajoutées ou modifiées à garder lors de la mise
+        à jour du fichier central.
 
     """
 
