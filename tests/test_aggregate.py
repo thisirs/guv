aaee5347bdac6205ee18e4d008e70df10a0d0883
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2024-11-01 20:07:28 +0100

    More tests

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -22,3 +22,32 @@ def test_aggregate_moodle_groups(guv, xlsx, guvcapfd, filename):
     columns_after = set(doc.df.columns)
 
     assert(columns_after - columns_before == {"Projet"})
+
+
+data = [("moodle_export_assignment.csv", ("Note",)),
+        ("moodle_export_gradebook_v1.xlsx", ("Test Quiz (Brut)",)),
+        ("moodle_export_gradebook_v2.xlsx", ("Test Quiz (Brut)",)),
+        ("moodle_export_gradebook_v3.xlsx", ("Test Quiz (Brut)",)),
+        ("moodle_export_quiz_v1.xlsx", ("Note/20,00",))]
+
+
+@path_dependency("test_xls_student_data")
+@pytest.mark.parametrize("filename,keep", data)
+def test_aggregate_moodle_grades(guv, xlsx, guvcapfd, filename, keep):
+    uv = guv.uvs[0]
+    guv.cd(guv.semester, uv)
+    guv.copy_file(filename, "documents")
+
+    guv.change_config(f"""
+    DOCS.aggregate_moodle_grades("documents/{filename}")
+    """)
+
+    doc = xlsx.tabular(guv.cwd / "effectif.xlsx")
+    columns_before = set(doc.df.columns)
+
+    guv().succeed()
+
+    doc = xlsx.tabular(guv.cwd / "effectif.xlsx")
+    columns_after = set(doc.df.columns)
+
+    assert(columns_after - columns_before == set(keep))
