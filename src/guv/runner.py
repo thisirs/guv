7595b415a713d78919c4b2fcdba380eb7714e876
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2025-05-07 12:07:08 +0200

    Simplify runner.py, load entry-points

--- src/guv/runner.py
+++ src/guv/runner.py
@@ -21,46 +21,6 @@ from .parser import get_parser
 from .tasks.base import SemesterTask, TaskBase, UVTask
 
 
-class ModuleTaskLoader(NamespaceTaskLoader):
-    @staticmethod
-    def from_modules(*modules):
-        return ModuleTaskLoader(*modules)
-
-    def __init__(self, *modules):
-        super().__init__()
-        self.namespace = {}
-        self.tasks = {}
-        self.variables = {}
-        self.load_modules(*modules)
-
-    def load_plugin_tasks(self):
-        tasks = []
-        for entry_point in importlib.metadata.entry_points(group="guv_tasks"):
-            name = entry_point.name
-            task_class = entry_point.load()
-            tasks.append((name, task_class))
-        logger.debug("%s tasks loaded from plugins", len(tasks))
-
-        self.tasks.update(dict(tasks))
-        self.namespace.update(dict(tasks))
-
-    def load_modules(self, *modules):
-        for module in modules:
-            m = inspect.getmembers(
-                module,
-                lambda v: inspect.isclass(v)
-                and issubclass(v, TaskBase)
-                and v not in [TaskBase, SemesterTask, UVTask],
-            )
-            logger.debug("%s tasks loaded from module `%s`", len(m), module)
-            self.tasks.update(dict(m))
-            self.namespace.update(dict(m))
-
-    def load_variables(self, dictionary):
-        self.variables.update(dictionary)
-        self.namespace.update(dictionary)
-
-
 def create_uv_dirs(base_dir, uvs):
     for uv in uvs:
         uv_dir = os.path.join(base_dir, uv)
@@ -160,62 +120,69 @@ def run_creastesemester(args):
         new_file.write(content)
 
     create_uv_dirs(base_dir, args.uv)
+    return 0
 
 
 def run_createuv(args):
     base_dir = os.getcwd()
     create_uv_dirs(base_dir, args.uv)
-
-
-def load_custom_tasks(filenames):
-    modules = []
-    for fn in filenames:
-        fp = os.path.join(settings.SEMESTER_DIR, fn)
-        if os.path.exists(fn):
-            module_name = os.path.splitext(os.path.basename(fp))[0]
-            spec = importlib.util.spec_from_file_location(module_name, fp)
-            module = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)
-            sys.modules[spec.name] = module
-            modules.append(module)
-        else:
-            raise FileNotFoundError("Le fichier de tâches n'existe pas :", fp)
-
-    return modules
-
-
-def run_doit(task_loader, args):
-    return DoitMain(task_loader).run(args)
+    return 0
 
 
 def get_task_loader():
-    task_loader = ModuleTaskLoader.from_modules(tasks)
+    task_loader = NamespaceTaskLoader()
+
+    # Load tasks from guv
+    m = inspect.getmembers(
+        tasks,
+        lambda v: inspect.isclass(v)
+        and issubclass(v, TaskBase)
+        and v not in [TaskBase, SemesterTask, UVTask],
+    )
+    logger.debug("%s core tasks loaded", len(m))
+    task_loader.namespace = dict(m)
+
+    # Load tasks from plugins
+    plugin_tasks = []
+    for entry_point in importlib.metadata.entry_points(group="guv_tasks"):
+        name = entry_point.name
+        task_class = entry_point.load()
+        plugin_tasks.append((name, task_class))
+
+    logger.debug("%s tasks loaded from plugins", len(plugin_tasks))
+    task_loader.namespace.update(dict(plugin_tasks))
+
     return task_loader
 
 
-def run_task(task_name):
-    """Load config.py files and run tasks."""
+def creastesemester_handler(other):
+    createsemester_parser = argparse.ArgumentParser(
+        prog="guv createsemester",
+        description="Crée un dossier de semestre",
+    )
+    createsemester_parser.add_argument("directory")
+    createsemester_parser.add_argument("--uv", nargs="*", default=[])
+    createsemester_parser.add_argument("--semester")
+    args = createsemester_parser.parse_args(other)
+    return run_creastesemester(args)
 
-    task_loader = get_task_loader()
 
-    # Add custom task and variables from config.py files
-    task_loader.load_variables(settings.settings)
-    modules = load_custom_tasks(settings.TASKS)
-    task_loader.load_modules(*modules)
-    task_loader.load_plugin_tasks()
+def createuv_handler(other):
+    logger.debug("Run createuv task")
+    createuv_parser = argparse.ArgumentParser(
+        prog="guv createuv",
+        description="Crée des dossiers d'UV"
+    )
+    createuv_parser.add_argument("uv", nargs="+")
+    args = createuv_parser.parse_args(other)
+    return run_createuv(args)
 
-    if task_name is None:
-        logger.debug("Run doit with default tasks")
-        return run_doit(task_loader, [])
 
-    if task_name == "doit":
-        logger.debug("Bypass call to doit")
-        return run_doit(task_loader, sys.argv[2:])
-
-    # Run doit without command-line arguments to avoid errors.
-    # Doit tasks can handle sys.argv themselves
-    logger.debug("Run doit with task only")
-    return run_doit(task_loader, [task_name])
+def get_handlers():
+    return {
+        "createsemester": creastesemester_handler,
+        "createuv": createuv_handler
+    }
 
 
 def print_completer(shell="zsh"):
@@ -305,35 +272,31 @@ def main(argv=sys.argv[1:]):
     parser.add_argument("command", nargs="?")
     args, other = parser.parse_known_args(argv)
 
+    task_name = args.command
     ret = None
     try:
-        if "-h" in other or "--help" in other:
-            task_loader = get_task_loader()
-            parser = get_parser(task_loader.tasks)
-            parser.parse_args(argv)
+        task_loader = get_task_loader()
+        handlers = get_handlers()
 
-        if args.command == "createsemester":
-            logger.debug("Run createsemester task")
-            createsemester_parser = argparse.ArgumentParser(
-                prog="guv createsemester",
-                description="Crée un dossier de semestre",
-            )
-            createsemester_parser.add_argument("directory")
-            createsemester_parser.add_argument("--uv", nargs="*", default=[])
-            createsemester_parser.add_argument("--semester")
-            args = createsemester_parser.parse_args(other)
-            ret = run_creastesemester(args)
-        elif args.command == "createuv":
-            logger.debug("Run createuv task")
-            createuv_parser = argparse.ArgumentParser(
-                prog="guv createuv",
-                description="Crée des dossiers d'UV"
-            )
-            createuv_parser.add_argument("uv", nargs="+")
-            args = createuv_parser.parse_args(other)
-            ret = run_createuv(args)
-        else:
-            ret = run_task(args.command)
+        if "-h" in other or "--help" in other:
+            parser = get_parser(task_loader.namespace)
+            parser.parse_args(argv)
+            return
+
+        elif task_name is None:
+            logger.debug("Run doit with default tasks")
+            ret = DoitMain(task_loader).run([])
+
+        elif task_name == "doit":
+            logger.debug("Bypass call to doit")
+            ret = DoitMain(task_loader).run(sys.argv[2:])
+
+        elif task_name in task_loader.namespace:
+            ret = DoitMain(task_loader).run([task_name])
+
+        elif task_name in handlers:
+            handler = handlers[task_name]
+            ret = handler(other)
 
     except Exception as e:
         if logger.level < logging.INFO:
