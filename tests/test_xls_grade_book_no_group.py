26ef474791616436d608359f01858097f6fbca54
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2025-06-03 18:36:09 +0200

    Test several yaml fragments in one file

--- tests/test_xls_grade_book_no_group.py
+++ tests/test_xls_grade_book_no_group.py
@@ -24,3 +24,13 @@ def test_xls_grade_book_no_group_2(guv):
         "xls_grade_book_no_group --name Test2 --marking-scheme documents/config_gradebook_test1.yaml,documents/config_gradebook_test2.yaml"
     ).succeed()
     guv.check_output_file(guv.cwd / "generated" / "Test2_gradebook.xlsx")
+
+@path_dependency("test_xls_student_data")
+def test_xls_grade_book_no_group_3(guv):
+    uv = guv.uvs[0]
+    guv.cd(guv.semester, uv)
+    guv.copy_file("config_gradebook_test3.yaml", "documents")
+    guv(
+        "xls_grade_book_no_group --name Test3 --marking-scheme documents/config_gradebook_test3.yaml"
+    ).succeed()
+    guv.check_output_file(guv.cwd / "generated" / "Test3_gradebook.xlsx")
