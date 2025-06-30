e7a85778a7adfa18e65cff887811d044c0be5ff9
Sylvain Rousseau <sylvain.rousseau@hds.utc.fr>
2025-06-30 20:29:01 +0200

    Add more tests on gradebooks

--- tests/test_xls_grade_book_group.py
+++ tests/test_xls_grade_book_group.py
@@ -3,12 +3,37 @@ from tests.plugins.test_path import path_dependency
 
 
 @path_dependency("test_xls_student_data")
-def test_xls_grade_book_group(guv, guvcapfd):
+def test_xls_grade_book_group_1(guv, guvcapfd):
     uv = guv.uvs[0]
     guv.cd(guv.semester, uv)
     guv.copy_file("config_gradebook_test1.yaml", "documents")
     guv(
-        "xls_grade_book_group --name Test -g Tutorial --marking-scheme documents/config_gradebook_test1.yaml"
+        "xls_grade_book_group --name Test_group_1 -g Tutorial --marking-scheme documents/config_gradebook_test1.yaml"
     ).succeed()
-    guv.check_output_file(guv.cwd / "generated" / "Test_gradebook.xlsx")
+    guv.check_output_file(guv.cwd / "generated" / "Test_group_1_gradebook.xlsx")
+    guvcapfd.no_warning()
+
+
+@path_dependency("test_xls_student_data")
+def test_xls_grade_book_group_2(guv, guvcapfd):
+    uv = guv.uvs[0]
+    guv.cd(guv.semester, uv)
+    guv.copy_file("config_gradebook_test1.yaml", "documents")
+    guv.copy_file("config_gradebook_test2.yaml", "documents")
+    guv(
+        "xls_grade_book_group --name Test_group_2 -g Tutorial --marking-scheme documents/config_gradebook_test1.yaml,documents/config_gradebook_test2.yaml"
+    ).succeed()
+    guv.check_output_file(guv.cwd / "generated" / "Test_group_2_gradebook.xlsx")
+    guvcapfd.no_warning()
+
+
+@path_dependency("test_xls_student_data")
+def test_xls_grade_book_group_3(guv, guvcapfd):
+    uv = guv.uvs[0]
+    guv.cd(guv.semester, uv)
+    guv.copy_file("config_gradebook_test3.yaml", "documents")
+    guv(
+        "xls_grade_book_group --name Test_group_3 -g Tutorial --marking-scheme documents/config_gradebook_test3.yaml"
+    ).succeed()
+    guv.check_output_file(guv.cwd / "generated" / "Test_group_3_gradebook.xlsx")
     guvcapfd.no_warning()
--- tests/test_xls_grade_book_no_group.py
+++ tests/test_xls_grade_book_no_group.py
@@ -7,12 +7,12 @@ def test_xls_grade_book_no_group_1(guv, guvcapfd):
     uv = guv.uvs[0]
     guv.cd(guv.semester, uv)
     guv.copy_file("config_gradebook_test1.yaml", "documents")
-    guv.copy_file("config_gradebook_test2.yaml", "documents")
     guv(
-        "xls_grade_book_no_group --name Test1 --marking-scheme documents/config_gradebook_test1.yaml,documents/config_gradebook_test2.yaml"
+        "xls_grade_book_no_group --name Test1 --marking-scheme documents/config_gradebook_test1.yaml"
     ).succeed()
     guv.check_output_file(guv.cwd / "generated" / "Test1_gradebook.xlsx")
     guvcapfd.no_warning()
+
 
 @path_dependency("test_xls_student_data")
 def test_xls_grade_book_no_group_2(guv, guvcapfd):
@@ -25,6 +25,7 @@ def test_xls_grade_book_no_group_2(guv, guvcapfd):
     ).succeed()
     guv.check_output_file(guv.cwd / "generated" / "Test2_gradebook.xlsx")
     guvcapfd.no_warning()
+
 
 @path_dependency("test_xls_student_data")
 def test_xls_grade_book_no_group_3(guv, guvcapfd):
